2a99a87aed70788f72eec8534827a675e6aaf9e8
Sanjay Joshi <joshisa@us.ibm.com>
2015-09-30 17:34:46 -0400

    Update storage_object.py

--- object_storage/storage_object.py
+++ object_storage/storage_object.py
@@ -385,9 +385,9 @@ class StorageObject:
             buff = data.read(4096)
         res = conn.finish()
 
+        print(checksum.hexdigest())
+        print(res.headers['Etag'])
         if check_md5:
-            print(checksum.hexdigest())
-            print(res.headers['Etag'])
             assert checksum.hexdigest() == res.headers['Etag'], \
                 'md5 hashes do not match'
         res.headers['Content-Length'] = transfered
